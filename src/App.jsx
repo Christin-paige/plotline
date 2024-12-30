6492c3cb0e848e81d347c172f3c65f00ffbca492
Christin-paige <christinpmartin@gmail.com>
2024-12-30 09:44:42 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LandingPage from './components/LandingPage'
+
+import './index.css'
+
+
+export default function App() {
+  return(
+    <>
+
+  <LandingPage />
+    </>
+  )
+}
+
+
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,8 @@
+export default function LandingPage(){
+    return(
+        <>
+        <h1 className="text-center">Plotline</h1>
+      
+      </>
+    )
+}
